88829b9d5d98ab287e1f797416ac4888d4d95330
ZenFarhat <s3777800@student.rmit.edu.au>
2021-02-03 15:41:35 +1100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Item from "./components/Item";
+
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <div className='app__itemsContainer'>
+        <Item
+          title='Model 3'
+          desc='Schedule a Touchless Test Drive'
+          descLink=''
+          backgroundImg={Model3}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='EXISTING INVENTORY'
+          rightBtnLink=''
+          twoButtons='true'
+          car='car'
+          first
+        />
+        <Item
+          title='Solar and Powerwall'
+          desc='Power Everything'
+          descLink=''
+          backgroundImg={SolarPanels}
+          leftBtnTxt='LEARN MORE'
+          leftBtnLink=''
+          rightBtnTxt=''
+          rightBtnLink=''
+          twoButtons=''
+        />
+        <Item
+          title='Model S'
+          desc='Schedule a Touchless Test Drive'
+          descLink=''
+          backgroundImg={ModelS}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='EXISTING INVENTORY'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Model X'
+          desc='Schedule a Touchless Test Drive'
+          descLink=''
+          backgroundImg={ModelX}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='EXISTING INVENTORY'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+        <Item
+          title='Model Y'
+          desc='Schedule a Touchless Test Drive'
+          descLink=''
+          backgroundImg={ModelY}
+          leftBtnTxt='CUSTOM ORDER'
+          leftBtnLink=''
+          rightBtnTxt='EXISTING INVENTORY'
+          rightBtnLink=''
+          twoButtons='true'
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h2>This is a CLONE not the REAL WEBSITE</h2>
+      <p>Created by Zen F, 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
